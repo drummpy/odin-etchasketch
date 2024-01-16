0631c8b9289e812f15300f9f4280477072bf3849
drummpy <154277179+drummpy@users.noreply.github.com>
2024-01-15 18:21:48 -0800

    Add create grid and remove grid functions

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function createGrid(size) {
+  for (let i = 0; i < size; i++) {
+    const gridRow = document.createElement("div");
+    createColumn(size, gridRow);
+    gridRow.setAttribute("id", "row");
+    gridRow.classList.add("grid-row");
+    boxContainer.appendChild(gridRow);
+  }
+}
+
+function createColumn(size, currentRow) {
+  for (let i = 0; i < size; i++) {
+    const gridColumn = document.createElement("div");
+    gridColumn.classList.add("grid-box");
+    currentRow.appendChild(gridColumn);
+  }
+}
+
+function removeGrid() {
+  while (boxContainer.firstChild) {
+    boxContainer.removeChild(boxContainer.firstChild);
+  }
+}
+
 function setSize() {
   let newSize = prompt("Enter your new grid size", "16");
 
@@ -11,10 +35,16 @@ const setSizeButton = document.querySelector("#set-size-button");
 const resetButton = document.querySelector("#reset-button");
 
 let gridSize = 16;
+createGrid(gridSize);
+
 setSizeButton.addEventListener("click", () => {
   gridSize = setSize();
+  removeGrid();
+  createGrid(gridSize);
   console.log(gridSize);
 });
 resetButton.addEventListener("click", () => {
-  console.log("Reset");
+  removeGrid();
+  createGrid(gridSize);
+  console.log("reset");
 });
